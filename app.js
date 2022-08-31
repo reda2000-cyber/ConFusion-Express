0befdd7951e4a1f6fade8c75c9a8c88e5b24dd10
reda-cyber <redasmaidi2000@gmail.com>
2022-08-31 22:01:05 +0200

    Express Generator REST API

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dishRouter = require("./routes/dishRouter.js");
+var leaderRouter = require('./routes/leaderRouter');
+var promotionRouter = require('./routes/promotionRouter');
 
 var app = express();
 
@@ -21,6 +24,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes', dishRouter.dishRouter);
+app.use('/dishes', dishRouter.dishRouterID);
+app.use('/leaders', leaderRouter.leaderRouter);
+app.use('/leaders', leaderRouter.leaderRouterID);
+app.use('/promotions', promotionRouter.promotionRouter);
+app.use('/promotions', promotionRouter.promotionRouterID);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
